2cc831db2d3f9c46f1547997dba207ec43f142a6
Cali93 <c.armut93@outlook.com>
2018-02-03 16:19:27 +0100

    initial config commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const expressGraphQL = require ('express-graphql');
+const schema = require ('./schema.js');
+
+
+const app = express();
+
+const port = process.env.PORT || 4000
+
+app.use('/graphql', expressGraphQL({
+  schema:schema,
+  graphiql:true
+}))
+
+app.listen(port, ()=>{
+  console.log('Server is running on port' + port)
+})
